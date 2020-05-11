4deb03a443c7c1d08a69ba4d31ab907efa8e17e7
lukas <lukas@ubuntu-server-check_mk>
2020-05-11 11:16:18 +0200

    second

--- script.sh
+++ script.sh
@@ -9,14 +9,62 @@ echo "######################################################"
 #Check for root rights
 if [ "$EUID" -ne 0 ]
 then 
-	echo "Please run as root or with sudo"
-    exit
+			echo "Please run as root or with sudo"
+			    exit
 fi
+
+#Git repo
+git_repo=https://github.com/lucki1000/wireguard-installer.git
+
+#Set wireguard port
+read -p "Enter the port you wish to run wireguard on it, default is 51820: " port
+
+#Set default interface IP
+read -p "Set Interface IP, default is 10.1.1.1: " interface_ip
+
+#Check if variable empty
+if [[ -z "$port" ]]; then
+			port=51820
+fi
+
+if [[ -z "$interface_ip" ]]; then
+			interface_ip=10.1.1.1
+fi
+
+#Set IP and netmask
+ip_client=$interface_ip
+ip_client+=/
+
+#Set netmask
+read -p "Enter the netmask, default is 32" ip_client_mask
+
+#Check for empty variable
+if [[ -z "$ip_client_mask" ]]; then
+			ip_client_mask=32
+fi
+
+ip_client+=$ip_client_mask
 
 #Create private and public key
 cd /etc/wireguard/
 umask 077
 wg genkey | tee privatekey | wg pubkey > publickey
 
+#Pivate Key
+pir_key=$(cat privatekey)
+
+#Public Key
+pub_key=$(cat publickey)
+
 #Create wg0.conf config file
-cp -r  /etc/wireguard/wg0.conf
+cat <<EOF>>wg0.conf
+[interface]
+Address = $interface_ip
+PrivateKey = $pri_key
+ListenPort = $port
+
+[Peer]
+PublicKey = $pub_key
+AllowedIPs = $ip_client
+EOF
+
